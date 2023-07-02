e00246cd4a2005e6a8b9f2fe1db1569ae6ac00cd
miroz devkota <mirozxy@gmail.com>
2023-07-02 20:58:28 +0545

    images fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,31 +3,31 @@ import Music from "@/components/Music";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-
 const api_key = "3121e81a9d9edade04ccdb1e7508f35e";
-const api_shared_key = "f2a40d5eb25e82f46eb8beeb8eb4e08d"
-const user_name = "amrit_ghimire"
+const api_shared_key = "f2a40d5eb25e82f46eb8beeb8eb4e08d";
+const user_name = "amrit_ghimire";
 
 async function getMusic() {
- const {data} = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${api_key}&format=json`)
+  const { data } = await axios.get(
+    `http://ws.audioscrobbler.com/2.0/?method=album.search&album=Believe&api_key=${api_key}&format=json`
+  );
 
-
-
- return data;
+  console.log(data);
+  return data;
 }
-
 
 export default function Home() {
   const { data, isLoading, isFetching, error } = useQuery(["music"], getMusic);
 
-  // console.log(data.tracks.track);
+  console.log({ data });
 
   if (isLoading) return <div className="max-[40px]">Loading...</div>;
 
   if (error) return <div className="max-[40px]">error...</div>;
   return (
-    <div className="grid gap-8 cols-4 max-w-[90%] m-auto ">
-      {data.tracks.track?.map((music: any) => {
+    <div className="flex border-2 border-red-200 p-40 flex-wrap justify-evenly gap-10">
+      {data?.results?.albummatches?.album?.map((music: any) => {
+        console.log("what is music", music);
         return <Music key={music.name} music={music} />;
       })}
     </div>
--- components/Music.tsx
+++ components/Music.tsx
@@ -1,26 +1,34 @@
-"use client"
-import { FC } from 'react'
-import Image from 'next/image'
+"use client";
+import { FC } from "react";
+import Image from "next/image";
 
 interface MusicProps {
-  music:any
+  music: any;
 }
 
-const Music: FC<MusicProps> = ({music}) => {
-  
-  return <div className='flex flex-col gap-2'>
-    <div className="relative w-[14rem] h-[14rem] overflow-hidden rounded-md">
-    <Image src={music.image[2]['#text']}
-          alt='music'  layout="fill" objectFit='cover' priority/>
+const Music: FC<MusicProps> = ({ music }) => {
+  console.log("image", music);
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="relative w-[14rem] h-[14rem] overflow-hidden rounded-md">
+        <Image
+          src={music.image[2]["#text"]}
+          alt="music"
+          layout="fill"
+          objectFit="cover"
+          priority
+        />
+      </div>
+
+      <div className="  ">
+        <p className="font-semibold text-lg w-44  text-ellipsis truncate">
+          {" "}
+          {music.name}
+        </p>
+        <p className="text-gray-400 text-sm">By {music.artist.name}</p>
+      </div>
     </div>
+  );
+};
 
-    <div className="  ">
-      <p className='font-semibold text-lg'> {music.name}</p>
-      <p className='text-gray-400 text-sm'>By {music.artist.name}</p>
-    </div>
-
-   
-  </div>
-}
-
-export default Music 
+export default Music;
